82f701c0640569af7bfa6027e0b689882bca5378
Lingfeng Fu <2871618714@qq.com>
2025-04-05 20:54:34 +0800

    send npc messages

---
+++ Assets/_DeepChat/Scripts/Data/EmoticonBank.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace _DeepChat.Scripts.Data
+{
+    [CreateAssetMenu(fileName = "EmoticonBank", menuName = "SO/EmoticonBank")]
+    public class EmoticonBank : ScriptableObject
+    {
+        public List<string> emoticons = new();
+    }
+}
---
+++ Assets/_DeepChat/Scripts/Logic/ChatGameCtrl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using _DeepChat.Scripts.Common;
+using _DeepChat.Scripts.Data;
+using _DeepChat.Scripts.ViewCtrls;
+using NaughtyAttributes;
+using UnityEngine;
+
+namespace _DeepChat.Scripts.Logic
+{
+    public class ChatGameCtrl : MonoBehaviour
+    {
+        [SerializeField] private MessageListViewCtrl messages;
+        [SerializeField] private float npcMessageSpan = 5.0f;
+        [SerializeField] private EmoticonBank npcMessageBank;
+        [SerializeField] private bool runOnStart;
+
+        private bool _isRunning;
+
+        private void Start()
+        {
+            if (!runOnStart)
+                return;
+            StartCoroutine(Run());
+        }
+
+        private IEnumerator Run()
+        {
+            _isRunning = true;
+            var wait = new WaitForSeconds(npcMessageSpan);
+            while (_isRunning)
+            {
+                yield return wait;
+                var index = Random.Range(0, npcMessageBank.emoticons.Count);
+                var emoticon = npcMessageBank.emoticons[index];
+                messages.Append(ActorType.Npc, emoticon);
+            }
+        }
+
+        [Button, ShowIf("_isRunning")]
+        private void Stop()
+        {
+            _isRunning = false;
+        }
+    }
+}
