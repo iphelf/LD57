e3b3634007f9bc42ae234cdb444ec26e33959c0f
tx <>
2025-04-07 10:17:24 +0800

    增加动效

--- Assets/_DeepChat/Scripts/ViewCtrls/ScoreViewCtrl.cs
+++ Assets/_DeepChat/Scripts/ViewCtrls/ScoreViewCtrl.cs
@@ -1,15 +1,18 @@
 ﻿using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace _DeepChat.Scripts.ViewCtrls
 {
     public class ScoreViewCtrl : MonoBehaviour
     {
         [SerializeField] private TMP_Text text;
-
+        
+        public UnityEvent OnScoreChanged;
         public void UpdateScore(int score, int maxScore)
         {
             text.text = $"{score}/{maxScore}";
+            OnScoreChanged?.Invoke();
         }
     }
 }
