009aa58652073ab6ce2959baff8e311d0a0aba73
Lingfeng Fu <2871618714@qq.com>
2025-04-07 19:36:37 +0800

    countdown fade out

--- Assets/_DeepChat/Scripts/ViewCtrls/CountdownViewCtrl.cs
+++ Assets/_DeepChat/Scripts/ViewCtrls/CountdownViewCtrl.cs
@@ -7,8 +7,10 @@ namespace _DeepChat.Scripts.ViewCtrls
     public class CountdownViewCtrl : MonoBehaviour
     {
         [SerializeField] private Image filler;
+        [SerializeField] private float fadeOutDuration = 0.5f;
 
         private bool _isRunning;
+        private bool _isFadingOut;
 
         private float _total;
         private float _remain;
@@ -18,6 +20,7 @@ namespace _DeepChat.Scripts.ViewCtrls
         {
             gameObject.SetActive(true);
             _isRunning = true;
+            _isFadingOut = false;
             _total = seconds;
             _remain = seconds;
             _callback = onTimeout;
@@ -28,26 +31,39 @@ namespace _DeepChat.Scripts.ViewCtrls
             gameObject.SetActive(false);
             _isRunning = false;
             _callback = null;
+            _isFadingOut = false;
         }
 
         private void Update()
         {
-            if (!_isRunning)
-                return;
-            _remain -= Time.deltaTime;
-
-            if (_remain > 0.0f)
+            if (_isRunning)
             {
-                filler.fillAmount = _remain / _total;
-                return;
+                _remain -= Time.deltaTime;
+
+                if (_remain > 0.0f)
+                {
+                    filler.fillAmount = _remain / _total;
+                    return;
+                }
+
+                filler.fillAmount = 0.0f;
+
+                _remain = 0.0f;
+                var callback = _callback;
+                _isRunning = false;
+                _callback = null;
+                _isFadingOut = true;
+                _remain = fadeOutDuration;
+                callback?.Invoke();
             }
-
-            filler.fillAmount = 0.0f;
-
-            _remain = 0.0f;
-            var callback = _callback;
-            StopCountdown();
-            callback?.Invoke();
+            else if (_isFadingOut)
+            {
+                _remain -= Time.deltaTime;
+                if (_remain > 0.0f)
+                    return;
+                gameObject.SetActive(false);
+                _isFadingOut = false;
+            }
         }
     }
 }
